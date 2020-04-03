8370509d1a853054ba6fbc22a48db4e7c9bb2901
Julie Gankina <qunya11@gmail.com>
2020-04-04 00:37:59 +0300

    export and import components

---
+++ src/components/utils.js
@@ -0,0 +1,6 @@
+import {Position} from "./const.js";
+
+
+const render = (container, template, place = Position.BEFOREEND) => container.insertAdjacentHTML(place, template);
+
+export default render;
